6143cc03c25a9ea3e53afbc35ffa7ff4d3ce0c0b
abraham <abrahampaucara>
2024-03-06 14:30:30 -0400

    homework 1 del M2

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,23 @@
-console.log(tempData);
+const filmsSection = document.getElementById('films');
+
+function renderFilms(movie){
+    const movieElement = document.createElement('article');
+    const containerMovie = document.createElement('div');
+    movieElement.classList.add('movie');
+    containerMovie.classList.add('divMovie');
+
+    movieElement.innerHTML = `<img src="${movie.poster}" alt="${movie.title}"/>`;
+
+    containerMovie.innerHTML = `
+    <a href="#"><h3>${movie.title} (${movie.year})</h3></a>
+    <p><strong>Director:</strong> ${movie.director}</p>
+    <p><strong>Duracion:</strong> ${movie.duration}</p>
+    <p><strong>Genero:</strong> ${movie.genre.join(', ')}</p>
+    <p><strong>Rate:</strong> ${movie.rate}</p>
+    `
+
+    filmsSection.appendChild(movieElement);
+    movieElement.appendChild(containerMovie);
+}
+
+tempData.forEach(renderFilms);
