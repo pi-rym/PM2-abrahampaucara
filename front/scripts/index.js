f326541630258b12937cc9b3045e9d2382fd54cf
abraham <abrahampaucara>
2024-03-08 00:36:10 -0400

    homework4 de M2

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,24 +1,6 @@
-const filmsSection = document.getElementById('films');
+/* const filmsSection = document.getElementById('films'); */
 
-function renderFilms(movie){
-    const movieElement = document.createElement('article');
-    const containerMovie = document.createElement('div');
-    movieElement.classList.add('movie');
-    containerMovie.classList.add('divMovie');
-
-    movieElement.innerHTML = `<img src="${movie.poster}" alt="${movie.title}"/>`;
-
-    containerMovie.innerHTML = `
-    <a href="#"><h3>${movie.title} (${movie.year})</h3></a>
-    <p><strong>Director:</strong> ${movie.director}</p>
-    <p><strong>Duracion:</strong> ${movie.duration}</p>
-    <p><strong>Genero:</strong> ${movie.genre.join(', ')}</p>
-    <p><strong>Rate:</strong> ${movie.rate}</p>
-    `
-
-    filmsSection.appendChild(movieElement);
-    movieElement.appendChild(containerMovie);
-}
+const renderFilms = require('./renderFilms');
 
 //tempData.forEach(renderFilms);
 $.get('https://students-api.2.us-1.fl0.io/movies', (data) => data.forEach(renderFilms));
---
+++ front/scripts/renderFilms.js
@@ -0,0 +1,23 @@
+function renderFilms(movie){
+    const filmsSection = document.getElementById('films');
+    
+    const movieElement = document.createElement('article');
+    const containerMovie = document.createElement('div');
+    movieElement.classList.add('movie');
+    containerMovie.classList.add('divMovie');
+
+    movieElement.innerHTML = `<img src="${movie.poster}" alt="${movie.title}"/>`;
+
+    containerMovie.innerHTML = `
+    <a href="#"><h3>${movie.title} (${movie.year})</h3></a>
+    <p><strong>Director:</strong> ${movie.director}</p>
+    <p><strong>Duracion:</strong> ${movie.duration}</p>
+    <p><strong>Genero:</strong> ${movie.genre.join(', ')}</p>
+    <p><strong>Rate:</strong> ${movie.rate}</p>
+    `
+
+    filmsSection.appendChild(movieElement);
+    movieElement.appendChild(containerMovie);
+}
+
+module.exports = renderFilms;
